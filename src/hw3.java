8ec55b31e7409eb86a7f43a22e6aef6b007d9874
Лямец Алексей <65312091+aliamets@users.noreply.github.com>
2020-05-22 16:39:54 +0300

    Initial commit

---
+++ src/hw3.java
@@ -0,0 +1,13 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class hw3 {
+    static Random random;
+    static Scanner sc;
+
+    public static void main(String[] args) {
+    }
+}
